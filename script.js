15bb2e12e0ef2cdb97a9d729736695447ef3ae50
Tim Zhou <timothy.zhou1@gmail.com>
2026-01-25 18:23:37 +1100

    begin work on improving UI display of weather data

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-let result;
-let destination;
-
 let locationInput = document.querySelector('#location-input');
 let submitBtn = document.querySelector('#submit-btn');
 let HTMLBody = document.querySelector('body');
 let weatherDataContainer = document.querySelector('.weather-data-container');
-let toggleFCBtn = document.querySelector('#toggleFC');
+// let toggleTemperatureTypeBtn = document.querySelector('#toggleFC');
+let weatherDataContent = document.querySelector('.weather-data-content');
 
 function getDayFromDate(date) {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -18,16 +16,17 @@ function convertFarenheitToCelsius(F) {
     return Math.round( (F - 32) * 5 / 9 );
 }
 
-//current task to work on next: clean up the code, i was able to get async function working, there is old code that uses .then and stuff like that which i dont need anymore
+//current task to work: display weather data in more visually appealing, user-friendly manner, like cards
 
 //note about async functions that i found out: running a function that is declared outside the async function will not follow the await feature, it seems to run the function immediately and not wait for fetch data to be retrieved *see note 2:
 submitBtn.addEventListener('click', async () => {
         let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationInput.value}?key=H9AE4P4A3CCXQ59FW9XNW6TUZ`);
         let weatherData = await response.json();
+        console.log(weatherData);
 
         //filters weatherData into an object with only the data I need to use (when i tried wrapping this in a self invoking function it didnt work for me)
         let weatherDataArray = weatherData.days.map (item => { 
-            return {date: item.datetime, day: getDayFromDate(item.datetime), temperatureF: item.temp, temperatureC: convertFarenheitToCelsius(item.temp)}  
+            return {date: item.datetime, day: getDayFromDate(item.datetime), temperatureF: item.temp, temperatureC: convertFarenheitToCelsius(item.temp), conditions: item.conditions}  
         });
 
         //*note 2: for example, this function was originally declared outside the async function but trying to run it that way caused it to run before fetching weather data was finished
@@ -38,56 +37,41 @@ submitBtn.addEventListener('click', async () => {
 
             for (const arrItem of weatherDataArray) {
                 const newDiv = document.createElement('div');
-                newDiv.textContent = `Date: ${arrItem.date} || Day: ${arrItem.day} || Temp: ${arrItem.temperatureC}C`;
+                newDiv.innerHTML = `
+                    <p> Date: ${arrItem.date} </p> 
+                    <p> Day: ${arrItem.day} </p> 
+                    <p> Temp: ${arrItem.temperatureC}C </p> 
+                    <p> Conditions: ${arrItem.conditions} </p>
+                `;                
                 weatherDataContainer.appendChild(newDiv);
             }
         })()
 
 });
 
+toggleTemperatureTypeBtn.addEventListener('click', () => {
 
-
-function getWeatherData() {
-    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationInput.value}?key=H9AE4P4A3CCXQ59FW9XNW6TUZ`)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(response) {
-      result = response.days;
-      console.log(response);
-      console.log('destination: ' + response.resolvedAddress);
-      let weatherDataArray = result.map (item => {
-          return { 
-            date: item.datetime, day: getDayFromDate(item.datetime), temperatureF: item.temp, 
-            temperatureC: convertFarenheitToCelsius(item.temp)
-          };  
-      });
-      console.log(weatherDataArray);
-      return weatherDataArray;
-    })
-    .catch(function(err) {
-        // Error :(
-        console.log(err);
-    });
-}
-//current task: use async/await to first get fetch weatherAPI data, place it in an array -> then take that array data and display it on the HTML
-
-
+});
 
 
 /* to do: 
-1. add 'days' based on date (done)
-2. have a button that toggles between displaying F and C temperatures
-3. add a function that clears the weather data being currently displayed (done)
-4. add an event listener so that when 'Enter/Return' button on keyboard is pressed, it also acts as if submit btn was clicked
+1. have the weather data displayed in a more visually appealing user friendly manner, like cards
+2. add an event listener so that when 'Enter/Return' button on keyboard is pressed, it also acts as if submit btn was clicked
+
+optional:
+1. have a button that toggles between displaying F and C temperatures
+
 */
 
 //overall task to work on now: Display the (weather) information on your webpage!
 
 
 
+
 /* --Completed Task Log--
+-task: add a function that clears the weather data being currently displayed (done)
+-task: add 'days' based on date (done)
 -task:  (DONE) Set up a form that will let users input their location and will fetch the weather info (still just console.log() it)
 -task: (DONE) take the response array object and filter it to return an array only with the data I need i.e. datetime and temp for each item   --DONE: used map array method to return an object for every arr item
-
+-task: (DONE) implemented async/await to fetch weather data array and use that array to display weather data on the HTML accordingly
  */
